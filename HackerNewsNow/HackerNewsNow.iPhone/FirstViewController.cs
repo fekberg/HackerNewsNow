fbf416db2a73b5ae852a8c4141c8ac997a7b4e81
Filip Ekberg <mail@filipekberg.se>
2014-04-03 17:00:13 +1100

    Added Table View to iPhone to dispaly top hits from HN

--- HackerNewsNow/HackerNewsNow.iPhone/FirstViewController.cs
+++ HackerNewsNow/HackerNewsNow.iPhone/FirstViewController.cs
@@ -1,56 +1,87 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using HackerNewsNow.Shared;
+using HackerNewsNow.Shared.Models;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 
 namespace HackerNewsNow.iPhone
 {
-	public partial class FirstViewController : UIViewController
+	public partial class FirstViewController : UITableViewController
 	{
+	    private HackerNewsTableSource source;
+	    private readonly IEntryRepository repository = new EntryRepository();
+
 		public FirstViewController (IntPtr handle) : base (handle)
 		{
 			Title = NSBundle.MainBundle.LocalizedString ("First", "First");
 			TabBarItem.Image = UIImage.FromBundle ("first");
 		}
 
-		public override void DidReceiveMemoryWarning ()
-		{
-			// Releases the view if it doesn't have a superview.
-		    base.DidReceiveMemoryWarning();
-
-		    // Release any cached data, images, etc that aren't in use.
-		}
-
-		#region View lifecycle
-
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
-			
-			// Perform any additional setup after loading the view, typically from a nib.
+
+            source = new HackerNewsTableSource(Enumerable.Empty<Entry>());
+		    TableView = new UITableView(Rectangle.Empty) {Source = source};
+
+		    RefreshControl = new UIRefreshControl();
+		    RefreshControl.ValueChanged += async (o, s) =>
+		    {
+		        await LoadEntriesAsync();
+                RefreshControl.EndRefreshing();
+		    };
+
+		    // Perform any additional setup after loading the view, typically from a nib.
 		}
 
-		public override void ViewWillAppear (bool animated)
-		{
-			base.ViewWillAppear (animated);
-		}
-
-		public override void ViewDidAppear (bool animated)
+		public async override void ViewDidAppear (bool animated)
 		{
 			base.ViewDidAppear (animated);
+
+            await LoadEntriesAsync();
 		}
 
-		public override void ViewWillDisappear (bool animated)
-		{
-			base.ViewWillDisappear (animated);
-		}
+	    private async Task LoadEntriesAsync()
+        {
+            var data = await repository.TopEntriesAsync();
 
-		public override void ViewDidDisappear (bool animated)
-		{
-			base.ViewDidDisappear (animated);
-		}
+            source.Entries = new List<Entry>(data);
 
-		#endregion
+            TableView.ReloadData();
+	    }
 	}
+
+    public class HackerNewsTableSource : UITableViewSource
+    {
+        public IList<Entry> Entries { get; set; }
+        private const string ValueCell = "Id";
+
+        public HackerNewsTableSource(IEnumerable<Entry> entries)
+        {
+            Entries = new List<Entry>(entries);
+        }
+
+        public override int RowsInSection(UITableView tableview, int section)
+        {
+            return Entries.Count;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell(ValueCell) ??
+                       new UITableViewCell(UITableViewCellStyle.Default, ValueCell);
+
+            var entry = Entries[indexPath.Row];
+            cell.TextLabel.Text = entry.Title;
+            cell.DetailTextLabel.Text = string.Format("Posted by {0} with {1} points", entry.PostedBy, entry.Points);
+
+            return cell;
+        }
+    }
 }
 
